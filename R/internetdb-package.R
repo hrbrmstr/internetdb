3b05c219172543b792c5729d02b823b35024f0c0
hrbrmstr <bob@rud.is>
2022-06-20 09:53:14 -0400

    R package repo initialization complete

---
+++ R/internetdb-package.R
@@ -0,0 +1,10 @@
+#' ...
+#' 
+#' @md
+#' @name internetdb
+#' @keywords internal
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+# @ useDynLib internetdb, .registration = TRUE
+"_PACKAGE"
